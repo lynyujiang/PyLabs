404025ca5098e3b5ed27d828a865fa6e530ec01c
lynyujiang <lynyujiang@aliyun.com>
2021-01-24 10:52:43 +0800

    Initial commit

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # PyLabs
 
 Python Learning Lab
+
+## 
--- scripts/github-repo-create.sh
+++ scripts/github-repo-create.sh
@@ -2,5 +2,5 @@
 
 # Creating a Github repo without using the website
 git init && git add . && git commit --allow-empty -m "Initial commit"
-gh repo create "lynyujiang/PyLabs" -d "Python Learning Lab" --public
+gh repo create "lynyujiang/A" -d "A" --public
 git push --set-upstream origin master
